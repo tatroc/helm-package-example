7b9741ce69483a3cf17181621fde4dd2470f1a28
Chris Tatro <christopher.tatro@kaplan.edu>
2022-11-04 15:12:30 -0500

    new helm package

--- test.sh
+++ test.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-./package.sh --environment prd \
---cluster cluster2 \
---region us-east-1 \
---variants prd \
---cluster-type common \
---application argocd
+./package.sh --environment prd --cluster cluster2 --region us-east-1 --variants prd --cluster-type common --application argocd
